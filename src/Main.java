6eebd18ffb5bf53143fc70617736a98642c498a1
EdnilsonJ <dongato1117@gmail.com>
2026-09-16 01:21:31 -0500

    Version 2: correccion del signo de la formula general

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,8 +10,8 @@ public class Main {
         b = sc.nextInt();
         System.out.print("Ingrese c: ");
         c = sc.nextInt();
-        x1 = (b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
-        x2 = (b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+        x1 = (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+        x2 = (-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
         System.out.println("Raiz 1: " + x1);
         System.out.println("Raiz 2: " + x2);
     }
